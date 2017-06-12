159e69ed5c37ba30805d261d77a795e9265731a6
= <=>
2017-06-12 14:28:40 +0200

    Functions done for getting the first "n" probable keysizes using the averages of "m" keysize normalized edit distance.

--- Set 1/repeatingKeyXORbreak.py
+++ Set 1/repeatingKeyXORbreak.py
@@ -1,5 +1,7 @@
 import base64
+import operator
 
+import repeatingKeyXOR as encrypt
 
 # Calculates the "edit distance" between two bytes.
 def calcEditDistByte(byte1, byte2):
@@ -37,33 +39,72 @@ def calcEditDist(bytes1, bytes2):
     for pos in range(0, len(bytes1)):
         editDist += calcEditDistByte(bytes1[pos], bytes2[pos])  # Calculate the distance between these integers
                                                                 # then increment the editDist by that amount.
-
+    #print(editDist)
     return editDist
 
 # Calculates the "normalized edit distance" for a given cipher and keysize.
-# Only uses the first 2 keysize size of bytes. Can expand on it if needed.
-def getNormEditDist(cipher, keysize):
-    return calcEditDist(cipher[0:keysize:], cipher[keysize:2*keysize])/keysize
+# Uses the first "times" keysizes and averages them.
+def getNormEditDist(cipher, keysize, times):
+    distance = 0
+
+    for num in range(0,times):
+        distance += calcEditDist(cipher[num*2*keysize:keysize*(2*num+1):], cipher[keysize*(2*num+1):(num+1)*2*keysize])
+
+    return distance/(times*keysize)
 
 # Gets the probable keysize between minkeysize and maxkeysize.
-# Only returns the "most probable". Can expand on it to return more.
-def getProbKeysize(cipher, minkeysize, maxkeysize):
-    probEditDist = 0
-    probKeysize = 0
+# Returns the "keys" most probable keysizes.
+def getProbKeysize(cipher, minkeysize, maxkeysize, keys, averages):
+    editDistkeysizedict = {}
+    probKeys = []
 
     for keysize in range(minkeysize, maxkeysize+1):
-        normEditDist = getNormEditDist(cipher, keysize)
+        normEditDist = getNormEditDist(cipher, keysize, averages)
+        #print("%d: %lf" % (keysize, normEditDist))
+        editDistkeysizedict[keysize]=normEditDist
 
-        if normEditDist>probEditDist:
-            probEditDist = normEditDist
-            probKeysize = keysize
+    # Make a sorted list out of the dictionary.
+    sorted_dict = sorted(editDistkeysizedict.items(), key=operator.itemgetter(1))
+    sorted_dict = sorted_dict[:keys]    # Get the first "keys" number of elements.
+    for elem in sorted_dict:
+        probKeys.append(elem[0])    # Add the keysize to the list we want to return.
 
-    return probKeysize
+    return probKeys
 
-string1 = "this is a test"
-string2 = "wokka wokka!!!"
+# Needs an array of bytes, and returns the most probable key byte.
+def breakSingleXOR(hex):
+    keyByte = 0
 
-testdistance = calcStrEditDist(string1, string2)
-print(testdistance)
+    # Loop through cipher bytes from 0 to ff(255, but range excludes the upper bound.)
+    for cipher in range(0, 256):
+        counter = 0
 
+        for num in hex:  # Loop through the bytes in the hex array.
+            decrypted = num ^ cipher  # XOR with the current cipher.
 
+            # If its a character, add it to the tmpstring.
+            if ((decrypted>0x40)and(decrypted<0x5b))or((decrypted>0x60)and(decrypted<0x7b))or(decrypted==0x20):
+                    counter += 1
+
+        # Choose the string with the most number of characters.
+        if counter > chosencounter:  # Note: This code overwrites the "most realistic" string of each input string.
+            chosencounter = counter  # If there is more than one encrypted string, only the last of them will be shown.
+            keyByte = num
+            counter = 0
+
+    return keyByte
+
+#string1 = "this is a test"
+#string2 = "wokka wokka!!!"     # Code testing the basic edit distance algorithm.
+
+#testdistance = calcStrEditDist(string1, string2)
+#print(testdistance)
+
+string = "Is post each that just leaf no. He connection interested so we an sympathize advantages. To said is it shed want do. Occasional middletons everything so to. Have spot part for his quit may. Enable it is square my an regard. Often merit stuff first oh up hills as he. Servants contempt as although addition dashwood is procured. Interest in yourself an do of numerous feelings cheerful confined."
+key = "test"
+
+cipher = encrypt.repeatingKeyXOR(string, key)
+
+print(cipher)
+
+print(getProbKeysize(cipher, 2, 15, 3, 4))
